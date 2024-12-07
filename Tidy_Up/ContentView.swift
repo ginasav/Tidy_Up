5aa7f7c124f8431499ba0bb95f0c33ce1461d24b
Gina Saviano <gina.saviano.97@gmail.com>
2024-12-07 15:00:13 +0100

    Initial Commit

---
+++ Tidy_Up/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Tidy_Up
+//
+//  Created by Gina Saviano on 07/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Tidy_Up/Tidy_UpApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tidy_UpApp.swift
+//  Tidy_Up
+//
+//  Created by Gina Saviano on 07/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Tidy_UpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
