b4145d32fcfa915ff79b62c0cae0c682bb554af6
Gina Saviano <gina.saviano.97@gmail.com>
2024-12-12 15:16:22 +0100

    Created the function to calculate how many books fit in the shelf in ShelfViewModel

--- Tidy_Up/ViewModel/ShelfViewModel.swift
+++ Tidy_Up/ViewModel/ShelfViewModel.swift
@@ -11,10 +11,20 @@ import SwiftUI
 @Observable class ShelfViewModel: ObservableObject {
     
     var books : [BookModel] = []
+    var booksThaFit: [(book: BookModel, count: Int)] = []
     
     // Functions connected to the Book Class
     func addBook(_ book: BookModel) {
         self.books.insert(book, at: 0)
     }
+    
+    // Calculate how many books fit in the given shelf length
+    func calculateBooksThatFit(for shelfLength: Double) {
+            let shelfLengthInCm = shelfLength * 100 // Convert meters to cm
+            booksThatFit = books.map { book in
+                let count = Int(shelfLengthInCm / book.thickness)
+                return (book, count)
+            }
+        }
     
 }
