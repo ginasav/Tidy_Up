2667c7d7e8c676c34854fd6c42b9e48c84cb4511
Gina Saviano <gina.saviano.97@gmail.com>
2024-12-14 16:19:01 +0100

    no message

---
+++ Tidy_Up/Views/LibraryView.swift
@@ -0,0 +1,85 @@
+//
+//  Untitled.swift
+//  Tidy_Up
+//
+//  Created by Gina Saviano on 07/12/24.
+//
+
+import SwiftUI
+
+struct LibraryView: View {
+    
+    @State var showModal: Bool = false
+    @EnvironmentObject var shelfViewModel: ShelfViewModel
+    
+    //MARK: BODY
+    var body: some View {
+        
+        NavigationView {
+            ZStack {
+                Color.background.ignoresSafeArea()
+                
+                VStack {
+                    
+                    if shelfViewModel.books.isEmpty {
+                        
+                        Text("No Book Added. Click + to add books to your shelves.")
+                            .foregroundStyle(.gray).opacity(0.8)
+                            .multilineTextAlignment(.center)
+                            .font(.title2)
+                            .padding(.horizontal, 100)
+                    } else {
+                        List {
+                            ForEach(shelfViewModel.books) { book in
+                                
+                                HStack {
+                                    VStack(alignment: .leading) {
+                                        Text(book.title)
+                                            .font(.headline)
+                                        Text("by \(book.author)")
+                                            .font(.subheadline)
+                                    }
+                                    
+                                    Spacer()
+                                    
+                                    Text("\(book.thickness, specifier: "%.2f") cm")
+                                        .font(.headline)
+                                }
+                                .foregroundStyle(.white)
+                            }
+                            .listRowBackground(Color.gray)
+                        }
+                        .scrollContentBackground(.hidden)
+                    }
+                } //make it changable from library
+            }
+            .navigationBarTitle("My Library")
+            .navigationBarTitleTextColor(.white)
+            .toolbar {
+                ToolbarItem (placement: .navigationBarTrailing) {
+                    Button (action: {
+                        showModal = true
+                    }) {
+                        Image(systemName: "plus")
+                            .font(.title2)
+                    }
+                    
+                }
+            }
+            .sheet(isPresented: $showModal) {
+                BooksInputView(showModal: $showModal, addBook: { book in
+                    shelfViewModel.addBook(book)
+                })
+            }
+        }
+    } //MARK: - END OF BODY
+}
+
+
+
+//MARK: PREVIEW
+#Preview {
+    LibraryView()
+        .environmentObject(ShelfViewModel())
+}
+
---
+++ Tidy_Up/Views/SplashScreenView.swift
@@ -0,0 +1,93 @@
+//
+//  LoadingView.swift
+//  Tidy_Up
+//
+//  Created by Gina Saviano on 07/12/24.
+//
+
+import SwiftUI
+
+struct SplashScreenView: View {
+    
+    @State private var showAnimation: Bool = false
+    @State private var scale = 0.6
+    @State private var opacity = 0.0
+    @ObservedObject var coordinator: AppCoordinator
+    
+    //MARK: BODY
+    var body: some View {
+        
+        
+        ZStack {
+            Color.background.ignoresSafeArea()
+            
+            VStack {
+                HStack(alignment: .bottom) {
+                    Image("Book1")
+                        .offset(x: 0, y: showAnimation ? 0 : -50)
+                        .animation(
+                            .interpolatingSpring(stiffness: 100, damping: 5)
+                            .repeatForever(autoreverses: false).delay(0.02),
+                            value: showAnimation
+                        )
+                    
+                    Image("Book3")
+                        .offset(x: 0, y: showAnimation ? 0 : -50)
+                        .animation(
+                            .interpolatingSpring(stiffness: 100, damping: 5)
+                            .repeatForever(autoreverses: false).delay(0.04),
+                            value: showAnimation
+                        )
+                    
+                    Image("Book2")
+                        .offset(x: 0, y: showAnimation ? 0 : -50)
+                        .animation(
+                            .interpolatingSpring(stiffness: 100, damping: 5)
+                            .repeatForever(autoreverses: false).delay(0.06),
+                            value: showAnimation
+                        )
+                    
+                    Image("Book4")
+                        .offset(x: 0, y: showAnimation ? 0 : -50)
+                        .animation(
+                            .interpolatingSpring(stiffness: 100, damping: 5)
+                            .repeatForever(autoreverses: false).delay(0.08),
+                            value: showAnimation
+                        )
+                }
+                
+                
+                Text("Tidy Up")
+                    .foregroundStyle(Color.accentColor)
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .padding()
+                    .offset(x: 0, y: showAnimation ? 0 : -50)
+                    .animation(
+                        .interpolatingSpring(stiffness: 100, damping: 5)
+                        .repeatForever(autoreverses: false).delay(0.02),
+                        value: showAnimation
+                    )
+            }.scaleEffect(0.6)
+                .opacity(opacity)
+            
+                .onAppear() {
+                    self.showAnimation.toggle()
+                    withAnimation(.easeIn(duration: 1.2)) {
+                        self.scale = 1.0
+                        self.opacity = 1.0
+                    }
+                    
+                    // Transition to the main screen
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+                        coordinator.switchToMainView()
+                    }
+                }
+        }
+    }
+}//MARK: - END OF BODY
+
+//MARK: PREVIEW
+//#Preview {
+//    SplashScreenView(coordinator: coordinator)
+//}
